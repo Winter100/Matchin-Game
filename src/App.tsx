12d9a92b0e4750a2e11d45d8848dd5825a3fedba
winter100 <100taeng@gmail.com>
2024-07-23 23:15:12 +0900

    feat: 위치 맞추기 기본로직 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,88 @@
+import { useRef, useState } from "react";
 import styled from "styled-components";
-import "./App.css";
+import DragAndDropList from "./compoents/DragAndDropList";
 
-const Section = styled.div`
+const Section = styled.section`
+  border: 1px solid black;
+  padding: 1rem;
   display: flex;
-  gap: 0.5rem;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 1rem;
+`;
+
+const Box = styled.div`
   border: 1px solid black;
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+export default function App() {
+  const [arr, setArr] = useState([1, 2, 3]);
+  const startIdx = useRef(0);
+  const endIdx = useRef(0);
 
-const Title = styled.h1`
-  color: green;
-  font-size: 5rem;
-`;
-
-const Input = styled.input<{ $inputColor?: string }>`
-  padding: 0.5rem;
-  color: ${(props) => props.$inputColor || "red"};
-`;
-
-const Button = styled.button<{ $primary?: boolean }>`
-  font-size: 3rem;
-  color: ${(props) => (props.$primary ? "green" : "#BF4F74")};
-`;
-
-function App() {
   return (
     <Section>
-      <Title>테스트</Title>
-      <Input defaultValue="기본값" type="text" />
-      <Input defaultValue="기본값" type="button" />
-      <Input type="checkbox" />
-      <Button>테스트 버튼1</Button>
-      <Button $primary>테스트 버튼2</Button>
+      <DragAndDropList />
     </Section>
   );
 }
+// import { useRef, useState } from "react";
 
-export default App;
+// export default function MoveBox() {
+//   const dragItem = useRef(0);
+//   const dragOverItem = useRef(0);
+//   const [list, setList] = useState([
+//     "Item 1",
+//     "Item 2",
+//     "Item 3",
+//     "Item 4",
+//     "Item 5",
+//     "Item 6",
+//   ]);
+
+//   const dragStart = (idx: number) => {
+//     dragItem.current = idx;
+//   };
+
+//   const dragEnter = (idx: number) => {
+//     dragOverItem.current = idx;
+//   };
+
+//   const drop = () => {
+//     const copyListItems = [...list];
+//     const dragItemContent = copyListItems[dragItem.current];
+//     copyListItems.splice(dragItem.current, 1);
+//     copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+//     dragItem.current = 0;
+//     dragOverItem.current = 0;
+//     setList(copyListItems);
+//     console.log("드랍");
+//   };
+//   return (
+//     <>
+//       {list &&
+//         list.map((item, index) => (
+//           <div
+//             style={{
+//               backgroundColor: "lightblue",
+//               margin: "20px 25%",
+//               textAlign: "center",
+//               fontSize: "40px",
+//             }}
+//             onDragStart={() => dragStart(index)}
+//             onDragEnter={() => dragEnter(index)}
+//             onDragOver={(e) => e.preventDefault()}
+//             onDragEnd={drop}
+//             key={index}
+//             draggable
+//           >
+//             {item}
+//           </div>
+//         ))}
+//     </>
+//   );
+// }
